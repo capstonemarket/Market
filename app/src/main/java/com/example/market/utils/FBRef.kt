462eaacf0bf527464b3f83006c96b453cf32f503
RainRang <jasd0622@hanmail.net>
2022-05-18 13:51:17 +0900

    필요없는 레퍼런스 정리(모델의 category 사용)

--- app/src/main/java/com/example/market/utils/FBRef.kt
+++ app/src/main/java/com/example/market/utils/FBRef.kt
@@ -9,13 +9,6 @@ class FBRef {
 
         val boardRef = database.getReference("board")
 
-        val c_clothes = database.getReference("c_clothes")
-        val c_electronics = database.getReference("c_electronics")
-        val c_book = database.getReference("c_book")
-        val c_food = database.getReference("c_food")
-        //"옷", "전자제품", "책", "음식" 카데고리별 저장 수정 필요
-
         val bookmarkRef = database.getReference("bookmark")
-        val upRef = database.getReference("up")
     }
 }
